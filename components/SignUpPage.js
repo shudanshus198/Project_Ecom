b899484bf292fc38795d5a3355d36b93c92024ce
Shudhanshu Choudhary <59770028+shudanshus198@users.noreply.github.com>
2022-08-08 09:32:10 +0530

    created file

---
+++ components/SignUpPage.js
@@ -0,0 +1,11 @@
+const SignUp = () => {
+    return ( 
+        <div className="w-full h-full bg-white bg-opacity-70 justify-center item-center">
+           <div> 
+              
+           </div>
+        </div>
+     );
+}
+ 
+export default SignUp;
